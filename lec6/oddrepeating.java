19aa2bf849044dd45650b254f17afeb167c3a83f
kastrahl <56175823+kastrahl@users.noreply.github.com>
2021-09-11 06:58:00 +0530

    Create oddrepeating.java
    5
    
    13579
    
    13579
    
    35791
    
    57913
    
    79135

---
+++ lec6/oddrepeating.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner str=new Scanner(System.in);
+		int n = str.nextInt();
+		int i ,j,nextnum=1;
+		for(i=1;i<=n;i++){
+		    for(j=1;j<=n;j++){
+		        System.out.print(nextnum);
+		        nextnum = nextnum +2 ;
+		        if(nextnum>=2*n)
+		            nextnum = nextnum%(2*n);
+		    }
+		    nextnum = i*2 - 1;
+		    System.out.println();
+		}
+	}
+}
