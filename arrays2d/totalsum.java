7aa18adb2dccfb1ccc5a3c33bf6452922d02e8b1
kasttech <72964665+kasttech@users.noreply.github.com>
2021-10-10 04:46:56 +0530

    Create totalsum.java

---
+++ arrays2d/totalsum.java
@@ -0,0 +1,36 @@
+public class Solution {
+
+	public static void totalSum(int[][] mat) {
+		int i,j,sum=0;
+        int rows=mat.length;
+        if(rows==0){
+            System.out.println("0");
+            return;
+        }
+        int cols=mat[0].length;
+        for(i=0;i<rows;i++){
+            for(j=0;j<cols;j++){
+                if(i==j){
+                    //System.out.println(mat[i][j]);
+                    sum+=mat[i][j];
+                    continue;
+                }else
+                if(i==0||j==0){
+                    //System.out.println(mat[i][j]);
+                    sum+=mat[i][j];
+                    continue;
+                }else
+                if(i==rows-1||j==cols-1){
+                    //System.out.println(mat[i][j]);
+                    sum+=mat[i][j];
+                    continue;
+                }else
+                    if(j==cols-1-i){
+                    	//System.out.println(mat[i][cols-1-i]+ " "+ j + " "+ (cols-i-1));
+                		sum+=mat[i][cols-1-i];
+                    }
+            }
+        }
+        System.out.println(sum);
+	}
+}
